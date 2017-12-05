c2aac4e0d76675bc072055ca3e0443735a47f6e1
lamak <max.lamak@gmail.com>
2017-12-06 00:00:16 +1000

    day 4 part 1

--- day4.py
+++ day4.py
@@ -1,9 +1,23 @@
 with open('day4.data') as f:
     raw = f.readlines()
 clean = [x.replace('\n', '') for x in raw]
+print(len(raw))
 counter = 0
+
 for i in clean:
     lst = list(i.split(' '))
     if len(lst) != len(set(lst)):
-        counter +=1
+        counter += 1
 print('Day 4, part 1: {}'.format(len(clean) - counter))
+
+charset = []
+counter = 0
+for string in clean:
+    lst = list(string.split(' '))
+    charset.append(list(''.join(sorted(word)) for word in lst))
+
+for i, element in enumerate(charset):
+    if len(element) == len(set(element)):
+        counter += 1
+
+print('Day 4, part 2: {}'.format(counter))
