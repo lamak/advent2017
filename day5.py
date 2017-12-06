ce41bef87af056ce6aa54eda099e65b75f905a64
Maksim Kapranov <max.lamak@gmail.com>
2017-12-06 11:11:25 +1000

    day 5 complete

--- day5.py
+++ day5.py
@@ -1,13 +1,26 @@
 f = open('day5.data').readlines()
 a = [int(row.strip()) for row in f]
+b = [int(row.strip()) for row in f]
 
 step = 0
 i = 0
 
 while i < len(a):
-    tmp = a [i]
-    a[i] = a[i] + 1
-    i = i + tmp
-    step = step + 1
-print(step, i)
+    tmp = a[i]
+    a[i] += 1
+    i += tmp
+    step += 1
+print('Day 5, part 1: {}', format(step))
 
+step = 0
+i = 0
+
+while i < len(b):
+    tmp = b[i]
+    if b[i] < 3:
+        b[i] += 1
+    else:
+        b[i] -= 1
+    i += tmp
+    step += 1
+print('Day 5, part 2: {}', format(step))
